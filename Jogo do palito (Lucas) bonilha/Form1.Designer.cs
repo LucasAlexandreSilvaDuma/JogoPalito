0bef6439cfc3679b52a5bc300e6f6d1f543fe453
Lucas Duma <lucasduma83@gmail.com>
2022-10-18 13:01:24 -0300

    first commit

---
+++ Jogo do palito (Lucas) bonilha/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Jogo_do_palito__Lucas__bonilha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelquantidade = new System.Windows.Forms.Label();
+            this.nudquantidade = new System.Windows.Forms.NumericUpDown();
+            this.btnjogar = new System.Windows.Forms.Button();
+            this.labelretirar = new System.Windows.Forms.Label();
+            this.mskdretirar = new System.Windows.Forms.MaskedTextBox();
+            this.btnretirar = new System.Windows.Forms.Button();
+            this.richTextBoxjogo = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudquantidade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelquantidade
+            // 
+            this.labelquantidade.AutoSize = true;
+            this.labelquantidade.Location = new System.Drawing.Point(63, 36);
+            this.labelquantidade.Name = "labelquantidade";
+            this.labelquantidade.Size = new System.Drawing.Size(161, 13);
+            this.labelquantidade.TabIndex = 0;
+            this.labelquantidade.Text = "Escolha a quantidade de palitos:";
+            // 
+            // nudquantidade
+            // 
+            this.nudquantidade.Location = new System.Drawing.Point(230, 34);
+            this.nudquantidade.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudquantidade.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudquantidade.Name = "nudquantidade";
+            this.nudquantidade.Size = new System.Drawing.Size(120, 20);
+            this.nudquantidade.TabIndex = 1;
+            this.nudquantidade.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // btnjogar
+            // 
+            this.btnjogar.Location = new System.Drawing.Point(372, 31);
+            this.btnjogar.Name = "btnjogar";
+            this.btnjogar.Size = new System.Drawing.Size(75, 23);
+            this.btnjogar.TabIndex = 2;
+            this.btnjogar.Text = "Jogar";
+            this.btnjogar.UseVisualStyleBackColor = true;
+            this.btnjogar.Click += new System.EventHandler(this.btnjogar_Click);
+            // 
+            // labelretirar
+            // 
+            this.labelretirar.AutoSize = true;
+            this.labelretirar.Location = new System.Drawing.Point(63, 88);
+            this.labelretirar.Name = "labelretirar";
+            this.labelretirar.Size = new System.Drawing.Size(149, 13);
+            this.labelretirar.TabIndex = 3;
+            this.labelretirar.Text = "Quantos palitos deseja retirar?";
+            this.labelretirar.Visible = false;
+            // 
+            // mskdretirar
+            // 
+            this.mskdretirar.Location = new System.Drawing.Point(230, 85);
+            this.mskdretirar.Mask = "0";
+            this.mskdretirar.Name = "mskdretirar";
+            this.mskdretirar.Size = new System.Drawing.Size(120, 20);
+            this.mskdretirar.TabIndex = 4;
+            this.mskdretirar.ValidatingType = typeof(int);
+            this.mskdretirar.Visible = false;
+            // 
+            // btnretirar
+            // 
+            this.btnretirar.Location = new System.Drawing.Point(372, 83);
+            this.btnretirar.Name = "btnretirar";
+            this.btnretirar.Size = new System.Drawing.Size(75, 23);
+            this.btnretirar.TabIndex = 5;
+            this.btnretirar.Text = "Retirar";
+            this.btnretirar.UseVisualStyleBackColor = true;
+            this.btnretirar.Visible = false;
+            this.btnretirar.Click += new System.EventHandler(this.btnretirar_Click);
+            // 
+            // richTextBoxjogo
+            // 
+            this.richTextBoxjogo.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBoxjogo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBoxjogo.Location = new System.Drawing.Point(0, 139);
+            this.richTextBoxjogo.Name = "richTextBoxjogo";
+            this.richTextBoxjogo.ReadOnly = true;
+            this.richTextBoxjogo.Size = new System.Drawing.Size(855, 190);
+            this.richTextBoxjogo.TabIndex = 6;
+            this.richTextBoxjogo.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(855, 329);
+            this.Controls.Add(this.richTextBoxjogo);
+            this.Controls.Add(this.btnretirar);
+            this.Controls.Add(this.mskdretirar);
+            this.Controls.Add(this.labelretirar);
+            this.Controls.Add(this.btnjogar);
+            this.Controls.Add(this.nudquantidade);
+            this.Controls.Add(this.labelquantidade);
+            this.Name = "Form1";
+            this.Text = "Jogo do Palito (Lucas)";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.nudquantidade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelquantidade;
+        private System.Windows.Forms.NumericUpDown nudquantidade;
+        private System.Windows.Forms.Button btnjogar;
+        private System.Windows.Forms.Label labelretirar;
+        private System.Windows.Forms.MaskedTextBox mskdretirar;
+        private System.Windows.Forms.Button btnretirar;
+        private System.Windows.Forms.RichTextBox richTextBoxjogo;
+    }
+}
+
---
+++ Jogo do palito (Lucas) bonilha/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_do_palito__Lucas__bonilha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            quantidadedepalitos++;
+            InitializeComponent();
+            CenterToScreen();
+
+        }
+
+        int quantidadedepalitos = 0;
+        
+        private void visibles()
+        {
+            quantidadedepalitos = 0;
+            labelretirar.Visible = true;
+            mskdretirar.Visible = true;
+            btnretirar.Visible = true;
+        }
+        private void enable()
+        {
+            labelquantidade.Enabled = false;
+            nudquantidade.Enabled = false;
+            btnjogar.Enabled = false;
+        }
+        private void jogadadobot()
+        {
+
+            quantidadedepalitos = (int)nudquantidade.Value;
+
+            mskdretirar.Focus();
+            int palitosbot = new Random().Next(1 , 3);
+           //(lógica - parte1)
+           
+            if (quantidadedepalitos % 2 == 0)
+            {
+                palitosbot = 1;
+                //quantidadedepalitos -= palitosbot;
+            }
+            if (quantidadedepalitos == 20)
+            {
+                palitosbot = 3;
+            }
+            if(quantidadedepalitos == 24)
+            {
+                palitosbot = 3;
+            }
+            if(quantidadedepalitos == 25)
+            {
+                palitosbot = 1;
+            }
+            if(quantidadedepalitos == 28)
+            {
+                palitosbot = 3;
+            }
+            if(quantidadedepalitos == 29)
+            {
+                palitosbot = 3;
+            }
+            quantidadedepalitos -= palitosbot;
+                richTextBoxjogo.AppendText("Eu começo !" + Environment.NewLine);
+                richTextBoxjogo.AppendText("Retirei " + palitosbot + " palito(s)" + Environment.NewLine);
+                richTextBoxjogo.AppendText("Sobraram " + quantidadedepalitos + " palito(s)" + Environment.NewLine + Environment.NewLine);
+                richTextBoxjogo.AppendText("Sua vez..." + Environment.NewLine + Environment.NewLine);
+                mskdretirar.Focus();
+        }
+        private void fimmdejogo()
+        {
+            labelretirar.Visible = false;
+            mskdretirar.Visible = false;
+            btnretirar.Visible = false;
+            ////
+            labelquantidade.Enabled = true;
+            nudquantidade.Enabled = true;
+            btnjogar.Enabled = true;
+            richTextBoxjogo.Clear();
+        }
+        private void Jogo()
+        {
+            if (string.IsNullOrEmpty(mskdretirar.Text))
+            {
+                MessageBox.Show("Preencha o campo", "Atenção");
+                return;
+            }
+
+            int palitosplayer1 = int.Parse(mskdretirar.Text);
+
+            if (palitosplayer1 < 1 || palitosplayer1 > 3)
+            {
+                MessageBox.Show("Só e possivel retirar de 1 a 3 palitos ", "Atenção");
+                return;
+            }
+            else if (palitosplayer1 > quantidadedepalitos)
+            {
+                MessageBox.Show("Digite um valor válido", "Atenção");
+                return;
+            }
+            else
+            {
+
+                quantidadedepalitos = quantidadedepalitos - palitosplayer1;
+
+                richTextBoxjogo.AppendText("Você retirou " + palitosplayer1 + " palito(s)" + Environment.NewLine);
+                richTextBoxjogo.AppendText("Sobraram " + quantidadedepalitos + " palito(s)" + Environment.NewLine + Environment.NewLine);
+                mskdretirar.Focus();
+                richTextBoxjogo.Focus();
+            }
+           
+
+            if (quantidadedepalitos == 0)
+            {
+                MessageBox.Show("Você perdeu", "Muito fácil");
+
+                if (MessageBox.Show("Gostaria de Jogar Novamente ?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    quantidadedepalitos = 0;
+                    fimmdejogo();
+                }
+                else
+                {
+                    Close();
+                }
+
+                return;
+            }
+
+            //(lógica - parte 2)
+            int jogadabot = 3;
+
+            int resto = quantidadedepalitos % 4;
+
+            if (resto != 0 && resto != 1)
+            {
+                jogadabot = resto - 1;
+            }
+            if(quantidadedepalitos == 25 || palitosplayer1 == 3)
+            {
+                jogadabot = 2;
+            }
+            //
+
+            quantidadedepalitos = quantidadedepalitos - jogadabot;
+
+            richTextBoxjogo.AppendText("Minha vez..." + Environment.NewLine);
+            richTextBoxjogo.AppendText("Retirei " + jogadabot + " palito(s)" + Environment.NewLine);
+            richTextBoxjogo.AppendText("Sobraram " + quantidadedepalitos + " palito(s)" + Environment.NewLine + Environment.NewLine);
+            richTextBoxjogo.AppendText("Sua vez..." + Environment.NewLine);
+            mskdretirar.Focus();
+
+           
+
+
+            if (quantidadedepalitos <= 0)
+            {
+                MessageBox.Show(" Como você conseguiu ganhar?", "Meu Deus");
+
+
+                if (MessageBox.Show("Gostaria de Jogar Novamente ?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    quantidadedepalitos = 0;
+                    fimmdejogo();
+                }
+                else
+                {
+                    Close();
+                }
+
+                return;
+            }
+            mskdretirar.Clear();
+
+        
+        }
+     
+    
+        private void btnjogar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                visibles();
+                enable();
+                jogadadobot();
+            }
+
+            catch
+            {
+                MessageBox.Show("Digite um valor válido", "Atenção");
+            }
+        }
+
+        private void btnretirar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Jogo();
+            }
+            catch
+            {
+            MessageBox.Show("Informe um valor válido", "Atenção");
+            }
+        }
+        
+
+        private void richTextBoxjogo_TextChanged(object sender, EventArgs e)
+        {
+            richTextBoxjogo.ScrollToCaret();
+        }
+
+       
+    }
+    
+}
+
+
+
+    
+
